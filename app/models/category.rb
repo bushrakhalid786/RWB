d88a82410ad513b4cf569868bb7290337256144e
prabu2006it <prabu2006it@gmail.com>
2017-08-04 23:02:57 +0530

    category rails admin changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,10 +5,4 @@ class Category < ApplicationRecord
   def html_view_enum
   	[['Detailed View', 0],['Customized View',1],['Normal View',2]]
   end
-
-  rails_admin do
-    create do 
-      field :html_view_enum
-    end
-  end
 end
