1779617aba2a9ff46161293936b31935fedc3df5
prabu2006it <prabu2006it@gmail.com>
2017-08-04 22:16:06 +0530

    pagination changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,10 +14,10 @@ class HomeController < ApplicationController
     if params[:child_alias_name].present?
       @category = Category.where(alias_name: params[:child_alias_name]).last      
       category_ids =  [@category.parent_id , @category.id]
-      @all_ads = Advertisement.by_categories(category_ids).paginate(page: params[:page],per_page: 10)
+      @all_ads = Advertisement.by_categories(category_ids).paginate(page: params[:page],per_page: 30)
     else
       @category = Category.where(alias_name: params[:alias_name] || params[:category]).last
-      @all_ads = Advertisement.by_categories([@category.id,@category.child_ids].flatten).paginate(page: params[:page],per_page: 10)
+      @all_ads = Advertisement.by_categories([@category.id,@category.child_ids].flatten).paginate(page: params[:page],per_page: 30)
     end
     if @category.present?
       render "show_category_page"
@@ -44,7 +44,7 @@ class HomeController < ApplicationController
                          with(:make, search_params[:make]) if search_params[:make].present?
                          with(:year, search_params[:year]) if search_params[:year].present?
                        end
-      @all_ads = advertisements.results.paginate(page: params[:page],per_page: 10)
+      @all_ads = advertisements.results.paginate(page: params[:page],per_page: 30)
     end
     render "show_category_page"
   end 
