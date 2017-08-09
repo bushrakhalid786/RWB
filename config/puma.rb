698cbe9b3ac544c50c7719a68a9a005e637d303e
prabu2006it <prabu2006it@gmail.com>
2017-08-10 00:42:20 +0530

    puma configuration removed

--- config/puma.rb
+++ config/puma.rb
@@ -1,26 +1,26 @@
-#!/usr/bin/env puma
+# #!/usr/bin/env puma
 
-directory '/home/deploy/regensburg/current'
-rackup "/home/deploy/regensburg/current/config.ru"
-environment 'production'
+# directory '/home/deploy/regensburg/current'
+# rackup "/home/deploy/regensburg/current/config.ru"
+# environment 'production'
 
-pidfile "/home/deploy/regensburg/shared/tmp/pids/puma.pid"
-state_path "/home/deploy/regensburg/shared/tmp/pids/puma.state"
-stdout_redirect '/home/deploy/regensburg/shared/log/puma_error.log', '/home/deploy/regensburg/shared/log/puma_access.log', true
-
-
-threads 0,8
-
-bind 'unix:///home/deploy/regensburg/shared/tmp/sockets/puma.sock'
-
-workers 0
+# pidfile "/home/deploy/regensburg/shared/tmp/pids/puma.pid"
+# state_path "/home/deploy/regensburg/shared/tmp/pids/puma.state"
+# stdout_redirect '/home/deploy/regensburg/shared/log/puma_error.log', '/home/deploy/regensburg/shared/log/puma_access.log', true
 
 
+# threads 0,8
 
-prune_bundler
+# bind 'unix:///home/deploy/regensburg/shared/tmp/sockets/puma.sock'
+
+# workers 0
 
 
-on_restart do
-  puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = "/home/deploy/regensburg/current/Gemfile"
-end
+
+# prune_bundler
+
+
+# on_restart do
+#   puts 'Refreshing Gemfile'
+#   ENV["BUNDLE_GEMFILE"] = "/home/deploy/regensburg/current/Gemfile"
+# end
