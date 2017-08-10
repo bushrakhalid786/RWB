f906b779729879f03138c24ebad88ac730a04e79
prabu2006it <prabu2006it@gmail.com>
2017-08-10 15:37:42 +0530

    envirnment variable changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
-server '13.58.225.207', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
