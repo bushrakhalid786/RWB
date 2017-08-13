e6329728d8ece0c4fa6c3eaad2c2e167dcb3b810
prabu2006it <prabu2006it@gmail.com>
2017-08-13 20:11:04 +0530

    devise changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   resources :categories
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_scope :user do
-  # using login path for registration
-    get '/users/sign_up' => 'registrations#new', :as => :registration
+    get '/sign_up' => 'registrations#new', :as => :custom_registration
   end
   devise_for :users
 
