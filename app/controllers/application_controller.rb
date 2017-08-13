941b5133be3bab65c9580ff6d5e340919db61ac9
prabu2006it <prabu2006it@gmail.com>
2017-08-13 18:30:41 +0530

    registration task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_menu
+  before_action :set_menu,:set_role
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.new_user_session_path, :alert => exception.message
   end
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
 
   def set_menu
   	@categories = Category.where(parent_id: nil)
+  end
+
+  def set_menu 
+    @normal_user_role = Role.where(name: "Normal User").last
   end
   
 end
