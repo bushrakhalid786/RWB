b810fe920aaaf74417f216a64b26fa474de88972
prabu2006it <prabu2006it@gmail.com>
2017-08-16 15:46:46 +0530

    puma configurationchanges

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,20 +40,20 @@ set :rvm_ruby_version, 'ruby-2.4.0' # Edit this if you are using MRI Ruby
 set :whenever_roles, -> { [:app, :db] }                                         # Allow crontab of these types of servers to be affected
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }      # Since we may share a single server (crontab instance) with many environments, scope whenever's crontab changes with this identifier so they updates from deployment of one environment don't overwrite another.
 
-# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-# set :puma_conf, "#{shared_path}/puma.rb"
-# set :puma_access_log, "#{shared_path}/log/puma_error.log"
-# set :puma_error_log, "#{shared_path}/log/puma_access.log"
-# set :puma_role, :app
-# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-# set :puma_threads, [4, 8]
-# set :puma_workers, 0
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true
-# set :puma_preload_app, false
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [4, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
 
 # before :deploy, :solr_stop
 # after :deploy, :solr_symlink
