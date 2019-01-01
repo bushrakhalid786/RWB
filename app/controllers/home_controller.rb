585d7d16bc09ef66a4fd8bba8a60f66184efb219
prabu2017 <prabu.gnanasekar@accionlabs.com>
2019-01-01 22:25:41 +0530

    more changes to ad page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,10 +14,10 @@ class HomeController < ApplicationController
     if params[:child_alias_name].present?
       @category = Category.where(alias_name: params[:child_alias_name]).last      
       category_ids =  [@category.parent_id , @category.id]
-      @all_ads = Advertisement.by_categories(category_ids).paginate(page: params[:page],per_page: 1)
+      @all_ads = Advertisement.by_categories(category_ids).paginate(page: params[:page],per_page: 6)
     else
       @category = Category.where(alias_name: params[:alias_name]).last
-      @all_ads = Advertisement.by_categories([@category.id,@category.child_ids].flatten).paginate(page: params[:page],per_page: 1)
+      @all_ads = Advertisement.by_categories([@category.id,@category.child_ids].flatten).paginate(page: params[:page],per_page: 6)
     end
     if @category.present?
       render "show_category_page"
@@ -29,7 +29,7 @@ class HomeController < ApplicationController
       search_params = params[:search].delete_if{|k,v| v.blank?}
       @all_ads = Advertisement.search do
                    fulltext(search_params["title"])
-                   paginate :page => params[:page], :per_page => 25
+                   paginate :page => params[:page], :per_page => 6
                  end.results
     end
     render "show_category_page"
